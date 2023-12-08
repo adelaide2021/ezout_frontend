c1d53d1ca7e2b3224c6a493950444b366531a8e6
adelaide2021 <xueyangli2015@gmail.com>
2023-12-07 16:45:06 -0800

    realize filter msg by shop_id

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -4,8 +4,6 @@ import { columns, dataFormat } from "./utils.jsx";
 import Socket from "./websocket";
 import "./index.css";
 
-// const socket = new Socket("ws://localhost:8080/ws/1");
-
 
 const Index = () => {
   let [data, setData] = useState([]);
@@ -13,28 +11,15 @@ const Index = () => {
 
   const socket = new Socket("ws://localhost:8080/ws/1");
 
-
-  // socket.addListener({
-  //   message: (mes) => {
-  //     console.log("print fron index.js: ", mes)
-  //     setLoading(false);
-  //     setData(mes);
-  //   },
-  // });
-  
-  // useEffect(() => {});
   useEffect(() => {
-    // 设置WebSocket侦听器
     socket.addListener({
       message: (mes) => {
         console.log("print fron index.js: ", mes);
         setLoading(false);
-        // 使用新数据更新状态
         setData(mes);
       },
     });
 
-    // 清理函数，在组件卸载时关闭WebSocket连接
     return () => {
       // socket.closeSocket();
     };
@@ -43,7 +28,10 @@ const Index = () => {
 
   const handelChange = (v) => {
     setLoading(true);
-    socket.send({ shopId: v.target.value });
+    // socket.send({ shopId: v.target.value });
+    const newShopId = v.target.value;
+    socket.send(JSON.stringify({ action: 'updateShopId', shopId: newShopId }));
+    console.log("from front en indes " + newShopId)
   };
 
   return (
@@ -58,7 +46,7 @@ const Index = () => {
               onBlur={handelChange}
               onKeyDown={(e) => {
                 console.log(e);
-                if (e.code === "Enter") {
+                if (e.key === "Enter") {
                   handelChange(e);
                 }
               }}
